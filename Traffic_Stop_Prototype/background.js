26a3bb295b9e5c18e938a2aef2b9d641675a8423
Al Abriam <abria004@cougars.csusm.edu>
2018-09-16 10:33:36 -0700

    Add files via upload

---
+++ Traffic_Stop_Prototype/background.js
@@ -0,0 +1,23 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#ff3030'}, function() {
+    console.log("The color is firebrick1.");
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlEquals: 'https://www.ebay.com/bhp/real-turtle-shell' },
+          }),
+              new chrome.declarativeContent.PageStateMatcher({
+                  pageUrl: { hostEquals: 'www.fursource.com' },
+              }),
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+  });
+});
